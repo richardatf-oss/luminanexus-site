b7c7cc42cd5cd850e1a56f67879362fe1b9f58a5
richardatf-oss <richardatf@gmail.com>
2026-03-17 17:07:00 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/global.css'
 
+/*
+  LuminaNexus entry point
+
+  This file does four simple things:
+  1. imports React
+  2. imports ReactDOM so the app can render in the browser
+  3. imports the main App component
+  4. imports the global stylesheet
+*/
+
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error(
+    'LuminaNexus could not find the root element. Make sure index.html contains <div id="root"></div>.'
+  )
+}
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
