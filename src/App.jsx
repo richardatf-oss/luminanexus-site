f64b14ac588b2203b57f5c08357c965b5e2ecf4f
richardatf-oss <richardatf@gmail.com>
2026-03-17 17:09:33 -0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from './components/Header'
+import Hero from './components/Hero'
+import TreeOfLife from './components/TreeOfLife'
+import Pillars from './components/Pillars'
+import FeatureSections from './components/FeatureSections'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+    <div className="site-shell">
+      <Header />
+
+      <main>
+        <Hero />
+        <TreeOfLife />
+        <Pillars />
+        <FeatureSections />
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
