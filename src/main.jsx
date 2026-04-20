cc22c933f7ec08d631cb0f7c60878020854a279c
richardatf-oss <richardatf@gmail.com>
2026-04-19 18:40:10 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,22 @@
 import React from 'react'
+import import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/global.css'
+
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error(
+    'LuminaNexus could not find the root element. Make sure index.html contains <div id="root"></div>.'
+  )
+}
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/global.css'
 
