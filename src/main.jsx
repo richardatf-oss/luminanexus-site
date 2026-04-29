80814894b2d9d9e11b5db65c8a0f98e384132950
richardatf-oss <richardatf@gmail.com>
2026-04-28 20:19:34 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './styles/global.css'
 
-const rootElement = document.getElementById('root')
-
-if (!rootElement) {
-  throw new Error(
-    'LuminaNexus could not find the root element. Make sure index.html contains <div id="root"></div>.'
-  )
-}
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
